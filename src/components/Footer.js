93ae4f49a3b26ae44ba4232a195bb920b66016cc
Chafiki <adamchafiki3@gmail.com>
2023-10-25 14:05:58 +0100

    change p

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -68,7 +68,7 @@ function Footer({ logo }) {
             </div>
           </div>
         </div>
-        <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
+        <hr className="my-6 border-gray-200 sm:mx-auto lg:my-7" />
         <div className="sm:flex sm:items-center sm:justify-between">
           <span className="text-sm text-gray-500 sm:text-center">
             © 2023{" "}
