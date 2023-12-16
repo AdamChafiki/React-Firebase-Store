39c4acbc7eb0141b999652ee0d92e2c0adbb7d15
Adam Chafiki <140613490+AdamChafiki@users.noreply.github.com>
2023-12-16 10:27:11 +0100

    Update fireBaseConfig.js

--- src/firebase/fireBaseConfig.js
+++ src/firebase/fireBaseConfig.js
@@ -6,13 +6,7 @@ import { getAuth,GoogleAuthProvider } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCYk6EmmzTHu5HGL32EkdGwmu7NcjyR9pA",
-  authDomain: "store-cd66e.firebaseapp.com",
-  projectId: "store-cd66e",
-  storageBucket: "store-cd66e.appspot.com",
-  messagingSenderId: "763203077211",
-  appId: "1:763203077211:web:7d9c59b0ea136610896b7e",
-  measurementId: "G-J4FDVE41M5"
+    // Copy your firebaseConfig here
 };
 
 // Initialize Firebase
